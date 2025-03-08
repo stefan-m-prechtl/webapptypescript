4e664b20da2d9a24b8e46dc90db766d4178c778a
Stefan <stefanmprechtle@web.de>
2025-03-08 13:01:28 +0100

    html-Template with expressions in lit-html

--- src/boundary/ui/userPresenter.ts
+++ src/boundary/ui/userPresenter.ts
@@ -3,42 +3,43 @@ import Model from "./userModel";
 import View from "./userView";
 
 export default class Presenter {
-    private baseURL = "http://localhost:8080/workflow";
-    
-    private view: View;
-    private model: Model;
-    
+  private baseURL = "http://localhost:8080/workflow";
 
-    constructor(view: View, model: Model) {
-        this.view = view;
-        this.model = model;
-        this.view.setPresenter(this);
+  private view: View;
+  private model: Model;
+
+  constructor(view: View, model: Model) {
+    this.view = view;
+    this.model = model;
+    this.view.setPresenter(this);
+  }
+
+  clear() {
+    this.model.reset();
+    this.view.clear();
+  }
+
+  async load() {
+    const service = new ServiceUser(this.baseURL);
+    const result = await service.getAll();
+
+    this.view.show(result);
+    result.forEach((user): void => {
+      this.model.addUser(user);
+    });
+  }
+
+  handleTableRowSelected(userid: string) {
+    console.log(`User with id ${userid} was selected`);
+    this.model.selectUser(Number(userid));
+
+    if (this.model.selectedUser) {
+      this.view.showCurrentUser(this.model.selectedUser);
     }
+  }
 
-    clear() {
-        this.model.reset();
-        this.view.clear();
-    }
-
-    async load() {
-        const service = new ServiceUser(this.baseURL);
-        const result = await service.getAll();
-
-        this.view.show(result);
-            result.forEach((user): void => {
-                this.model.addUser(user);
-                console.log(user.toJson());
-            });
-    }
-
-    handleTableRowSelected(userid: string) {
-        console.log(`User with id ${userid} was selected` );
-        this.model.selectUser(Number(userid));
-
-        if (this.model.selectedUser)
-        {
-            this.view.showCurrentUser(this.model.selectedUser);
-        }
-    }
-
+  handleActionEdit() {
+    this.model.selectUser(1);
+    this.view.showEditUser(this.model.selectedUser!);
+  }
 }
--- src/boundary/ui/userView.ts
+++ src/boundary/ui/userView.ts
@@ -1,137 +1,147 @@
 import { User } from "../../domain/user";
 import Presenter from "./userPresenter";
-import { html, render } from 'lit-html';
-
+import { html, render } from "lit-html";
+import { templateContent } from "lit-html/directives/template-content.js";
 
 type NullableHtmlElement = HTMLElement | null;
 
 export default class View {
+  presenter: Presenter | null;
+  rootElement: NullableHtmlElement;
+  tblBody: HTMLTableElement;
 
+  constructor(id: string) {
+    this.presenter = null;
 
-    presenter: Presenter | null;
-    rootElement: NullableHtmlElement;
-    tblBody: HTMLTableElement;
+    this.rootElement = document.querySelector<HTMLElement>(id)!;
+    this.tblBody = document.querySelector<HTMLTableElement>("#tblBody")!;
+    this.initView();
+    this.initEventHandler();
+  }
 
-    constructor(id: string) {
+  setPresenter(presenter: Presenter) {
+    this.presenter = presenter;
+  }
 
-        this.presenter = null;
+  initView() {}
 
-        this.rootElement = document.querySelector<HTMLElement>(id)!;
-        this.tblBody = document.querySelector<HTMLTableElement>("#tblBody")!;
-        this.initView();
-        this.initEventHandler();
+  initEventHandler() {
+    const btnLoad = document.querySelector<HTMLButtonElement>("#btnLoad")!;
+    const btnClear = document.querySelector<HTMLButtonElement>("#btnClear")!;
+    const chkSelectAll =
+      document.querySelector<HTMLInputElement>("#selectAll")!;
 
+    const actionEdit =
+      document.querySelector<HTMLButtonElement>("#actionEdit")!;
+
+    btnLoad.addEventListener("click", () => {
+      this.presenter?.load();
+    });
+
+    btnClear.addEventListener("click", () => {
+      this.presenter?.clear();
+    });
+
+    chkSelectAll.addEventListener("change", () => {
+      const checkboxes =
+        document.querySelectorAll<HTMLInputElement>(".row-checkbox");
+      checkboxes.forEach((checkbox) => {
+        checkbox.checked = chkSelectAll.checked;
+        checkbox
+          .closest("tr")
+          ?.classList.toggle("selectedRow", chkSelectAll.checked);
+      });
+      this.updateActionMenu(checkboxes);
+    });
+
+    actionEdit.addEventListener("click", () => {
+      this.presenter?.handleActionEdit();
+    });
+  }
+
+  updateActionMenu(checkboxes: NodeListOf<HTMLInputElement>) {
+    const actionsMenu = document.querySelector("#actions")!;
+
+    const anyChecked = Array.from(checkboxes).some(
+      (checkbox) => checkbox.checked
+    );
+    if (anyChecked) {
+      actionsMenu.removeAttribute("hidden");
+    } else {
+      actionsMenu.setAttribute("hidden", "true");
     }
+  }
 
-    setPresenter(presenter: Presenter) {
-        this.presenter = presenter;
-    }
+  clear() {
+    render(html``, this.tblBody);
+  }
 
-    initView() {
+  show(users: User[]) {
+    const templateTable = (users: User[]) =>
+      html` ${users.map(
+        (user) => html`<tr data-id="${user.id}">
+          <td><input type="checkbox" class="row-checkbox" /></td>
+          <td>${user.name}</td>
+          <td>${user.firstname}</td>
+          <td>${user.lastname}</td>
+        </tr>`
+      )}`;
+    render(templateTable(users), this.tblBody);
+    this.attachRowEventListeners();
+  }
 
+  attachRowEventListeners() {
+    const checkboxes =
+      document.querySelectorAll<HTMLInputElement>(".row-checkbox");
 
-    }
-
-    initEventHandler() {
-        const btnLoad = document.querySelector<HTMLButtonElement>("#btnLoad")!;
-        const btnClear = document.querySelector<HTMLButtonElement>("#btnClear")!;
-        const chkSelectAll = document.querySelector<HTMLInputElement>('#selectAll')!;
-
-        const actionEdit =document.querySelector<HTMLButtonElement>("#actionEdit")!;
-
-        btnLoad.addEventListener("click", () => {
-            this.presenter?.load();
-        });
-
-        btnClear.addEventListener("click", () => {
-            this.presenter?.clear();
-        });
-
-        chkSelectAll.addEventListener("change", () => {
-            const checkboxes = document.querySelectorAll<HTMLInputElement>(".row-checkbox");
-            checkboxes.forEach(checkbox => {
-                checkbox.checked = chkSelectAll.checked;
-                checkbox.closest("tr")?.classList.toggle("selectedRow", chkSelectAll.checked);
-            });
-            this.updateActionMenu(checkboxes);
-        });
-
-        actionEdit.addEventListener("click", () => {
-            const editDialog = document.querySelector<HTMLButtonElement>("#editdialog")!;
-            editDialog.style.display = "block"; 
-        });
-    }
-
-    updateActionMenu(checkboxes: NodeListOf<HTMLInputElement>) {
-
-        const actionsMenu = document.querySelector("#actions")!;
-
-        const anyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
-        if (anyChecked) {
-            actionsMenu.removeAttribute("hidden");
-        } else {
-            actionsMenu.setAttribute("hidden", "true");
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", (e) => {
+        const target = e.target as HTMLInputElement;
+        const row = target.closest("tr");
+        if (row) {
+          row.classList.toggle("selectedRow", target.checked);
         }
-    }
 
+        const chkSelectAll =
+          document.querySelector<HTMLInputElement>("#selectAll")!;
+        const allChecked = Array.from(checkboxes).every(
+          (checkbox) => checkbox.checked
+        );
+        if (allChecked) {
+          chkSelectAll.checked = true;
+          chkSelectAll.indeterminate = false;
+        } else {
+          const noneChecked = Array.from(checkboxes).every(
+            (checkbox) => !checkbox.checked
+          );
+          if (noneChecked) {
+            chkSelectAll.checked = false;
+            chkSelectAll.indeterminate = false;
+          } else {
+            chkSelectAll.checked = false;
+            chkSelectAll.indeterminate = true;
+          }
+        }
 
-    clear() {
-        render(html``, this.tblBody);
-    }
+        this.updateActionMenu(checkboxes);
+      });
+    });
+  }
 
-    show(users: User[]) {
+  showCurrentUser(user: User) {
+    console.log(`Current User: id=${user.id}, name=${user.name}`);
+  }
 
-        const templateTable = (users: User[]) => html`
-                ${users.map(user => html`<tr data-id="${user.id}">
-                    <td><input type="checkbox" class="row-checkbox"></td>
-                    <td>${user.name}</td>
-                    <td>${user.firstname}</td>
-                    <td>${user.lastname}</td>
-                </tr>`)}`;
-        render(templateTable(users), this.tblBody);
-        this.attachRowEventListeners();
-    }
+  showEditUser(user: User) {
+    console.log(`Current User: id=${user.id}, name=${user.name}`);
 
-    attachRowEventListeners() {
-        const checkboxes = document.querySelectorAll<HTMLInputElement>(".row-checkbox");
-
-        checkboxes.forEach(checkbox => {
-            checkbox.addEventListener("change", (e) => {
-                const target = e.target as HTMLInputElement;
-                const row = target.closest("tr");
-                if (row) {
-                    row.classList.toggle("selectedRow", target.checked);
-                }
-
-                const chkSelectAll = document.querySelector<HTMLInputElement>('#selectAll')!;
-                const allChecked = Array.from(checkboxes).every(checkbox => checkbox.checked);
-                if (allChecked) {
-                    
-                    chkSelectAll.checked = true;
-                    chkSelectAll.indeterminate=false;
-                }
-                else {
-                    const noneChecked = Array.from(checkboxes).every(checkbox => !checkbox.checked);
-                    if (noneChecked) {
-                        chkSelectAll.checked = false;
-                        chkSelectAll.indeterminate=false;
-                    }
-                    else
-                    {
-                        chkSelectAll.checked = false;
-                        chkSelectAll.indeterminate=true;
-                    }
-                }
-
-
-                this.updateActionMenu(checkboxes);
-            });
-        });
-    }
-
-    showCurrentUser(user: User) {
-        console.log(`Current User: id=${user.id}, name=${user.name}`)
-    }
+    const htmlTemplate = document.querySelector(
+      "#testtemplate"
+    ) as HTMLTemplateElement;
+    //const htmlCode = "<h1>Current User</h1><p>User: ${user.name}</p><p>Nachname: ${user.lastname}</p>";
+    const htmlCode = htmlTemplate.innerHTML;
+    const litTemplate = (user: User) => html`${eval("html`" + htmlCode + "`")}`;
+    const div = document.querySelector("#testdiv") as HTMLElement;
+    render(litTemplate(user), div);
+  }
 }
-
-
