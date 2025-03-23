b690457328236ff6b681685f5e0ecd8dc54d79c1
etienne <stefanmprechtl>
2025-03-23 13:27:39 +0100

    Layout

--- src/boundary/ui/user.component.edit.ts
+++ src/boundary/ui/user.component.edit.ts
@@ -19,12 +19,18 @@ export default class UserComponentEdit extends LitElement {
 
   render() {
     return html`
-      <div>
-        <p>Info zu Benutzer</p>
-        <p>Kennung: ${this.user?.name}</p>
-        <p>Vorname: ${this.user?.firstname}</p>
-        <p>Nachname: ${this.user?.lastname}</p>
-      </div>
+    <div >
+        <form class="w3-container w3-card-4 w3-light-grey w3-padding">
+        <h2>Info zum Benutzer</h2>
+          <label>Kennung:</label>
+          <input type="text" class="w3-input w3-border w3-margin-bottom" .value=${this.user?.name ?? ""}>
+          <label>Vorname:</label>
+          <input type="text" class="w3-input w3-border w3-margin-bottom" .value=${this.user?.firstname ?? ""}>
+          <label>Nachname:</label>
+          <input type="text" class="w3-input w3-border w3-margin-bottom" .value=${this.user?.lastname ?? ""}>
+        </form>
+      
+    </div>
     `;
   }
 }
